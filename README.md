a13f8b0cab910fc73bfd0f1efa7829c3345872b3
Phakphum Artkaew <pma5244@psu.edu>
2024-01-12 22:00:40 -0500

    website init

---
+++ README.md
@@ -0,0 +1,28 @@
+# Phakphum's Resume Website
+
+## Introduction
+This repository holds my unique resume website, a tribute to the classic Windows 95 interface with a twist of modern web development. It serves as a fun and interactive way to learn about my professional background in software engineering.
+
+## Special Thanks
+A heartfelt thank you to the creator of the original [Windows 95 UI Kit](https://github.com/DonChiaQE/win95) on GitHub. Their work inspired the aesthetic and functionality that make this site both nostalgic and engaging.
+
+## Features
+- **Classic Windows 95 Theme:** A creative and engaging way to present my professional profile.
+- **Interactive Experience:** Users can interact with desktop elements reminiscent of the classic Windows 95 interface.
+- **Ongoing Development:** A dedicated space indicating the ongoing addition of new content and features.
+
+## Technologies
+- **Vue.js:** For building a dynamic and responsive user interface.
+- **Vuex:** For efficient state management throughout the application.
+- **Classic Styling:** Using CSS and HTML to recreate the Windows 95 look and feel.
+
+## Getting Started
+To set up this project on your local machine:
+1. Clone the repository.
+2. Follow the setup instructions specified in the [Windows 95 UI Kit Readme](https://github.com/DonChiaQE/win95/blob/main/README.md).
+
+## Contribution
+Feedback and contributions are welcome as they help improve the site's functionality and user experience.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE) file for details.
---
+++ src/components/views/Bio.vue
@@ -0,0 +1,136 @@
+<template>
+  <div>
+    <img class="self" src="@/assets/Biography/self.jpeg" />
+    <h2 class="header">Phakphum Artkaew (ภาคภูมิ อาจแก้ว)</h2>
+    <h4 class="subtitle" style="padding-bottom: 10px">
+      1st year master’s student in computer engineering at NYU Tandon
+    </h4>
+    <h4 class="subtitle">Brooklyn, NY 📍</h4>
+    <div class="badge-grid">
+      <img
+        class="badge"
+        alt="C/C++"
+        src="https://img.shields.io/badge/-C%2FC%2B%2B-red"
+      />
+      <img
+        class="badge"
+        alt="Python"
+        src="https://img.shields.io/badge/-Python-blue"
+      />
+      <img
+        class="badge"
+        alt="Java"
+        src="https://img.shields.io/badge/-Java-orange"
+      />
+      <img
+        class="badge"
+        alt="JavaScript"
+        src="https://img.shields.io/badge/-Javascript-yellow"
+      />
+      <img
+        class="badge"
+        alt="Scala"
+        src="https://img.shields.io/badge/-Scala-maroon"
+      />
+      <img
+        class="badge"
+        alt="SQL"
+        src="https://img.shields.io/badge/-SQL-lightgrey"
+      />
+      <img
+        class="badge"
+        alt="Git"
+        src="https://img.shields.io/badge/-Git-critical"
+      />
+      <img
+        class="badge"
+        alt="Couchbase"
+        src="https://img.shields.io/badge/-Couchbase-crimson"
+      />
+      <img
+        class="badge"
+        alt="Kafka"
+        src="https://img.shields.io/badge/-Kafka-blueviolet"
+      />
+      <img
+        class="badge"
+        alt="HTML/CSS"
+        src="https://img.shields.io/badge/-HTML%2FCSS-yellowgreen"
+      />
+      <img
+        class="badge"
+        alt="Swift"
+        src="https://img.shields.io/badge/-Swift-3e8bc3"
+      />
+      <img
+        class="badge"
+        alt="Vue.js"
+        src="https://img.shields.io/badge/-VueJS-61b083"
+      />
+
+    </div>
+    <div class="inner-content">
+      <h3 class="heading"><b>About Me</b></h3>
+      <p class="paragraph">
+        👋 Hi! Hello there! I'm Phakphum Artkaew, and you've just landed on my personal resume website, currently a work in progress 🚧. Exciting updates and insights into my journey in software engineering and computer science are on the way. Stick around for more!
+      </p>
+    </div>
+    <div class="inner-content">
+      <h3 class="heading"><b>Résumé</b></h3>
+      <a
+        style="color: #ff5733"
+        href="/files/Resume_Phakphum.pdf"
+        class="paragraph"
+        >Check out my résumé here.</a
+      >
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.badge-grid {
+  display: table;
+  align-items: center;
+  margin-top: 20px;
+}
+
+.badge {
+  width: auto;
+  height: 20px;
+  border-radius: 0px;
+  font-size: 12px;
+  padding-right: 10px;
+  /* box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.12), 0 6px 20px 0 rgba(0, 0, 0, 0.1); */
+}
+
+.self {
+  width: 60%;
+  height: auto;
+  /* border-radius: 5px; */
+}
+
+.subtitle {
+  font-size: 12px;
+  color: gray;
+}
+
+.heading {
+  padding-bottom: 5px;
+}
+
+.header {
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+
+.inner-content {
+  padding-top: 30px;
+}
+
+h2,
+h4,
+h3 {
+  padding: 0;
+  margin: 0;
+}
+</style>
